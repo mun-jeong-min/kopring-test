9d9116340f9a66186ba5b5ecfefe5b9c620e645b
munjeongmin <3330kys@dsm.hs.kr>
2022-12-14 15:27:15 +0900

    firstcommit

---
+++ src/main/kotlin/com/example/kopringtest/KopringTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopringtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringTestApplication>(*args)
+}
